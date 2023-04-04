fb15ef20c8b131bf54508c47346d9f427b607309
Abhinav <abhimamidipaka@gmail.com>
2023-04-04 15:13:47 -0700

    change fetch

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 export const fetchResponse =  async(chat) => {
     try {
         // after depoloyment you should change the fetch URL below
-        const response = await fetch('http://localhost:5173', { 
+        const response = await fetch('https://earnest-torte-8ace06.netlify.app', { 
             method: 'POST',
             headers: {
                 "Content-Type": "application/json"
